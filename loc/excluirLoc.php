b75342f4d86980f58b6620a22a43eb753717ed2a
D. Salvador <disney.js@gmail.com>
2021-03-26 15:00:36 -0300

    Atualização 26MAR2021
    Arquivo com INSERTs para testes

--- loc/excluirLoc.php
+++ loc/excluirLoc.php
@@ -10,12 +10,12 @@
         
         $stm->bindValue(1,$codigo);
         
-        $res = $stm->execute();
-        $stm->closeCursor();
-        $stm=NULL;
-        $con=NULL; 
-    header("Location: locacao.php");
-    } 
+            $res = $stm->execute();
+            $stm->closeCursor();
+            $stm=NULL;
+            $con=NULL; 
+        header("Location: locacao.php");
+        }
         catch(PDOException $erro){
             echo "FALHA EM DELETAR A LOCAÇÃO: <br>" . $erro->getMessage();
             echo "<br><br>NÃO FOI POSSÍVEL REMOVER A LOCAÇÃO. VERIFIQUE SE HÁ PENDÊNCIAS.";
---
+++ sql/inserts_loc.sql
@@ -0,0 +1,17 @@
+INSERT INTO cliente (nome,telefone) VALUES ('Albert Einstein','(81)98765-0099');
+INSERT INTO cliente (nome,telefone) VALUES ('Nikola Tesla','(51)98457-0011');
+INSERT INTO cliente (nome,telefone) VALUES ('Marie Curie','(21)91234-4321');
+INSERT INTO cliente (nome,telefone) VALUES ('Alan Turing','(11)91912-1964');
+INSERT INTO cliente (nome,telefone) VALUES ('Ada Lovelace','(47)91815-1852');
+
+INSERT INTO veiculo (modelo,placa) VALUES ('Ford Maverick', 'ABC1234');
+INSERT INTO veiculo (modelo,placa) VALUES ('Chevrolet Opala', 'CBA4321');
+INSERT INTO veiculo (modelo,placa) VALUES ('VW Fusca', 'FSC1234');
+INSERT INTO veiculo (modelo,placa) VALUES ('Fiat 147', 'FIT0147');
+INSERT INTO veiculo (modelo,placa) VALUES ('Dodge Charger RT', 'CHR3690');
+
+
+INSERT INTO aluguel (datainicio, datafim, preco, situacao, cliente, veiculo) VALUES ('2021-03-25', '2021-03-26', 130, 'A', 4, 2);
+INSERT INTO aluguel (datainicio, datafim, preco, situacao, cliente, veiculo) VALUES ('2021-03-24', '2021-03-27', 100, 'A', 1, 3);
+INSERT INTO aluguel (datainicio, datafim, preco, situacao, cliente, veiculo) VALUES ('2021-03-24', '2021-03-25', 110, 'A', 2, 1);
+INSERT INTO aluguel (datainicio, datafim, preco, situacao, cliente, veiculo) VALUES ('2021-03-24', '2021-03-28', 90, 'A', 5, 4);
