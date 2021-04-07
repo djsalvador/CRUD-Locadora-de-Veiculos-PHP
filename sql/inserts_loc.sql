9cd25e38ec41c07308584f676043e53340d3cd63
D. Salvador <disney.js@gmail.com>
2021-04-07 14:26:05 -0300

    ALTs 07ABRIL2021
    Alterações cosméticas
    - Título
    - Observação
    - Dados no SQL

--- sql/inserts_loc.sql
+++ sql/inserts_loc.sql
@@ -1,8 +1,8 @@
-INSERT INTO cliente (nome,telefone) VALUES ('Albert Einstein','(81)98765-0099');
-INSERT INTO cliente (nome,telefone) VALUES ('Nikola Tesla','(51)98457-0011');
-INSERT INTO cliente (nome,telefone) VALUES ('Marie Curie','(21)91234-4321');
-INSERT INTO cliente (nome,telefone) VALUES ('Alan Turing','(11)91912-1964');
-INSERT INTO cliente (nome,telefone) VALUES ('Ada Lovelace','(47)91815-1852');
+INSERT INTO cliente (nome,telefone) VALUES ('Albert Einstein','(81)99991-0009');
+INSERT INTO cliente (nome,telefone) VALUES ('Nikola Tesla','(51)99992-0008');
+INSERT INTO cliente (nome,telefone) VALUES ('Marie Curie','(21)99993-0007');
+INSERT INTO cliente (nome,telefone) VALUES ('Alan Turing','(11)99994-0006');
+INSERT INTO cliente (nome,telefone) VALUES ('Ada Lovelace','(47)99995-0005');
 
 INSERT INTO veiculo (modelo,placa) VALUES ('Ford Maverick', 'ABC1234');
 INSERT INTO veiculo (modelo,placa) VALUES ('Chevrolet Opala', 'CBA4321');
@@ -11,7 +11,7 @@ INSERT INTO veiculo (modelo,placa) VALUES ('Fiat 147', 'FIT0147');
 INSERT INTO veiculo (modelo,placa) VALUES ('Dodge Charger RT', 'CHR3690');
 
 
-INSERT INTO aluguel (datainicio, datafim, preco, situacao, cliente, veiculo) VALUES ('2021-03-25', '2021-03-26', 130, 'A', 4, 2);
-INSERT INTO aluguel (datainicio, datafim, preco, situacao, cliente, veiculo) VALUES ('2021-03-24', '2021-03-27', 100, 'A', 1, 3);
-INSERT INTO aluguel (datainicio, datafim, preco, situacao, cliente, veiculo) VALUES ('2021-03-24', '2021-03-25', 110, 'A', 2, 1);
-INSERT INTO aluguel (datainicio, datafim, preco, situacao, cliente, veiculo) VALUES ('2021-03-24', '2021-03-28', 90, 'A', 5, 4);
+INSERT INTO aluguel (datainicio, datafim, preco, situacao, cliente, veiculo) VALUES ('2021-04-07', '2021-04-10', 130, 'A', 4, 2);
+INSERT INTO aluguel (datainicio, datafim, preco, situacao, cliente, veiculo) VALUES ('2021-04-07', '2021-04-11', 100, 'A', 1, 3);
+INSERT INTO aluguel (datainicio, datafim, preco, situacao, cliente, veiculo) VALUES ('2021-04-06', '2021-04-07', 110, 'A', 2, 1);
+INSERT INTO aluguel (datainicio, datafim, preco, situacao, cliente, veiculo) VALUES ('2021-04-05', '2021-04-08', 90, 'A', 5, 4);
